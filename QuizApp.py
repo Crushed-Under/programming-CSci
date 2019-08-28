b0663876c5f2bd4288af1f24cd754c41cba59746
Crushed-Under <47192323+Crushed-Under@users.noreply.github.com>
2019-08-28 14:37:14 +1200

    Fixed minor typo

--- QuizApp.py
+++ QuizApp.py
@@ -1,9 +1,9 @@
 from tkinter import *
-class QuizApp(object):
+class QuizApp():
     def __init__(self,parent):
         frame1=Frame(parent,width=80,height=80,bg="red")
 if __name__ == "__main__":
     root=Tk()
     app=QuizApp(root)
-    app.mainloop
+    root.mainloop
         
