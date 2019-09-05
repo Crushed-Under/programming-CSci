8350211ea59c944d905c21435d35de63cbe40f32
Crushed-Under <47192323+Crushed-Under@users.noreply.github.com>
2019-09-06 11:50:43 +1200

    Err:atmp, to lod Radiobtns frm nested lists

--- QuizApp.py
+++ QuizApp.py
@@ -1,9 +1,25 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+LARGEFONT=("Impact",72)
+SMALLFONT=("Verdana",18)
+qNa=(("a",1,2,3,4),("b",1,2,3))
 class QuizApp():
     def __init__(self,parent):
-        frame1=Frame(parent,width=80,height=80,bg="red")
+        self.v = tk.IntVar()
+        self.v.set(1)
+
+        rb_list=[]
+        for i in qNa[1[]]:
+            rb = tk.Radiobutton(parent, variable = self.v, value = i, text = "Option One")
+            rb_list.append(rb)
+            rb.pack()
+        label1 = tk.Label(parent, textvariable = self.v) #tkinter converts IntVar to text for textvariable
+
+        label1.pack()
+
+        
+        
 if __name__ == "__main__":
-    root=Tk()
+    root=tk.Tk()
     app=QuizApp(root)
     root.mainloop
-        
