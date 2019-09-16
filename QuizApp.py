cf60a4f0e7f337ea2153be12cefa4687a90326cd
Crushed-Under <47192323+Crushed-Under@users.noreply.github.com>
2019-09-16 12:06:59 +1200

    Fixed issue w/ syntax when ldng rad btns from list

--- QuizApp.py
+++ QuizApp.py
@@ -2,18 +2,22 @@ import tkinter as tk
 from tkinter import ttk
 LARGEFONT=("Impact",72)
 SMALLFONT=("Verdana",18)
-qNa=(("a",1,2,3,4),("b",1,2,3))
+
 class QuizApp():
     def __init__(self,parent):
         self.v = tk.IntVar()
         self.v.set(1)
-
+        self.qNa=(("a",1,2,3,4),("b",1,2,3))
+        self.counter=0
         rb_list=[]
-        for i in qNa[1[]]:
-            rb = tk.Radiobutton(parent, variable = self.v, value = i, text = "Option One")
-            rb_list.append(rb)
-            rb.pack()
-        label1 = tk.Label(parent, textvariable = self.v) #tkinter converts IntVar to text for textvariable
+        while True:
+
+            for i in self.qNa[counter][1:]:
+                rb = tk.Radiobutton(parent, variable = self.v, value = i, textvariable= self.)
+                rb_list.append(rb)
+                rb.pack()
+            counter+=1
+            label1 = tk.Label(parent, textvariable = self.v) #tkinter converts IntVar to text for textvariable
 
         label1.pack()
 
