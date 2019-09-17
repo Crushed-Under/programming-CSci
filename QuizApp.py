f219091fd8fa89bc7b2cf72bf0a4b808d0e1903d
Crushed-Under <47192323+Crushed-Under@users.noreply.github.com>
2019-09-17 18:57:12 +1200

    Checking commit works

--- QuizApp.py
+++ QuizApp.py
@@ -8,16 +8,19 @@ class QuizApp():
         self.v = tk.IntVar()
         self.v.set(1)
         self.qNa=(("a",1,2,3,4),("b",1,2,3))
-        self.counter=0
+        self.cnter=0
         rb_list=[]
         while True:
-
-            for i in self.qNa[counter][1:]:
-                rb = tk.Radiobutton(parent, variable = self.v, value = i, textvariable= self.)
-                rb_list.append(rb)
-                rb.pack()
-            counter+=1
-            label1 = tk.Label(parent, textvariable = self.v) #tkinter converts IntVar to text for textvariable
+            if self.cnter == len(self.qNa):
+                for i in self.qNa[self.cnter][1:]:
+                    rb = tk.Radiobutton(parent, variable = self.v, value = i, textvariable= i)
+                    rb_list.append(rb)
+                    rb.pack()
+                self.cntr+=1
+                btn1=tk.Button(text="Next",font=SMALLFONT,command=)
+            else:
+                break
+        label1 = tk.Label(parent, textvariable = self.v) #tkinter converts IntVar to text for textvariable
 
         label1.pack()
 
