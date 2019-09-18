1e44dfe680c068558dd07e0be88a60d59cb9bfee
Crushed-Under <47192323+Crushed-Under@users.noreply.github.com>
2019-09-18 21:14:35 +1200

    Fxd brkn sntax Put radio btns for loop in method

--- QuizApp.py
+++ QuizApp.py
@@ -11,20 +11,20 @@ class QuizApp():
         self.v.set("l")
         self.rb_list=[]
         self.attemptlist=[]
-        for i in self.qNa[self.cnter][1:]:#Iterates through inner lists of qNa 
-            self.rb = tk.Radiobutton(parent, variable = self.v, value = i, textvariable= i)
-            self.rb_list.append(self.rb)
-            self.rb.pack()
+        self.radioBtn()
         label1 = tk.Label(parent, textvariable = self.v) #tkinter converts IntVar to text for textvariable
         label1.pack()
-        nextbutton=tk.Button(text="Next",font=SMALLFONT,command=self.NextButtoncmd)
-        nextbutton.pack()
-        
-    def NextButtoncmd(self):
+        nxtbtn=tk.Button(text="Next",font=SMALLFONT,command=self.nxt_btn_cmd)
+        nxtbtn.pack()
+    def radioBtnGen(self):
+        for i in self.qNa[self.cnter][1:]:#Iterates through inner lists of qNa 
+            self.rb = tk.Radiobutton(variable = self.v, value = i, textvariable= i)
+            self.rb_list.append(self.rb)
+            self.rb.pack()   
+    def nxt_btn_cmd(self):
         a=self.v.get()
         self.attemptlist.append(a)
-        print(self.v)
-        print(self.attemptlist)
+
 
 if __name__ == "__main__":
     root=tk.Tk()
