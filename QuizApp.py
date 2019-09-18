dbb10073ec79dc820d4e172138f84faecc207330
Crushed-Under <47192323+Crushed-Under@users.noreply.github.com>
2019-09-18 20:26:34 +1200

    Using next command to confirm question

--- QuizApp.py
+++ QuizApp.py
@@ -1,31 +1,31 @@
+#Btn=Button cmd=command
 import tkinter as tk
-from tkinter import ttk
 LARGEFONT=("Impact",72)
 SMALLFONT=("Verdana",18)
 
 class QuizApp():
-    def __init__(self,parent):
-        self.v = tk.IntVar()
-        self.v.set(1)
-        self.qNa=(("a",1,2,3,4),("b",1,2,3))
-        self.cnter=0
-        rb_list=[]
-        while True:
-            if self.cnter == len(self.qNa):
-                for i in self.qNa[self.cnter][1:]:
-                    rb = tk.Radiobutton(parent, variable = self.v, value = i, textvariable= i)
-                    rb_list.append(rb)
-                    rb.pack()
-                self.cntr+=1
-                btn1=tk.Button(text="Next",font=SMALLFONT,command=)
-            else:
-                break
+    def __init__(self,parent):    
+        self.cnter=0 #Counter for iterating through questions
+        self.qNa=(("a","1","2","ey man","4"),("b","1","2","3"))#List of questions and answers
+        self.v = tk.StringVar()#Variable for radiobuttons
+        self.v.set("l")
+        self.rb_list=[]
+        self.attemptlist=[]
+        for i in self.qNa[self.cnter][1:]:#Iterates through inner lists of qNa 
+            self.rb = tk.Radiobutton(parent, variable = self.v, value = i, textvariable= i)
+            self.rb_list.append(self.rb)
+            self.rb.pack()
         label1 = tk.Label(parent, textvariable = self.v) #tkinter converts IntVar to text for textvariable
-
         label1.pack()
+        nextbutton=tk.Button(text="Next",font=SMALLFONT,command=self.NextButtoncmd)
+        nextbutton.pack()
+        
+    def NextButtoncmd(self):
+        a=self.v.get()
+        self.attemptlist.append(a)
+        print(self.v)
+        print(self.attemptlist)
 
-        
-        
 if __name__ == "__main__":
     root=tk.Tk()
     app=QuizApp(root)
