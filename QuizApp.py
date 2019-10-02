4a98ff7de93a1844fb47ebc14f284b0d12fd0a5d
Crushed-Under <47192323+Crushed-Under@users.noreply.github.com>
2019-10-02 14:40:54 +1300

    Attribution error

--- QuizApp.py
+++ QuizApp.py
@@ -7,14 +7,14 @@ class QuizApp():
     def __init__(self,parent):    
         self.qNa=(("What is the capital city of Puerto Rico","San Juan","Tiquana","Honduras"),
         ("What is the world's third largest sea?","The Mediterranean","The Tasman","The Black sea","The Caspian Sea"),
-        (" What is the capital of Qatar?","Abu Dubi","Dubai","Asur","Nineva"),
+        (" What is the capital of Qatar?","Abu Dubi","Dubai","Asur","Nineva","Doha"), 
         ("In which South American country is the Atacama desert?","Paraguay","Argentina","Chile","Ecuador"),
         ("What is the least populated state in the US","Kansas","Missisipi","Wyoming")
         )#List of questions and answers
-        self.correctanswers=("San Juan",)
+        self.correctanswers=("San Juan","The Mediterranean","Doha","Chile","Wyoming")#All the correct answers
         self.v = tk.StringVar()#Variable for radiobuttons
-        self.radiobtnsframe=0
         self.page=0
+        self.radiobtnsframe=0
         self.v.set("n/a")
         self.rblist=[]
         self.attemptlist=[]        
@@ -24,27 +24,29 @@ class QuizApp():
         self.wronglabel=tk.Label(font=SMALLFONT)
 
         self.confirmlabel = tk.Label(parent, textvariable = self.v) #tkinter converts IntVar to text for textvariable
-        self.confirmlabel.grid(row=1,column=0)
+        self.confirmlabel.grid(row=1,column=1)
 
-        self.questionlabel= tk.Label(text="",font=SMALLFONT)
+        self.questionlabel=tk.Label(textvariable=self.questionvar,font=SMALLFONT)
+        self.questionlabel.grid(row=0,column=0)
 
         nxtbtn=tk.Button(text="Next",font=SMALLFONT,command=self.nxt_btn_cmd)
-        nxtbtn.grid(row=3,column=0)
+        nxtbtn.grid(row=4,column=0)
 
         backbtn=tk.Button(text="Back",font=SMALLFONT,command=self.back_btn_cmd)
-        backbtn.grid(row=4,column=0)
+        backbtn.grid(row=5,column=0)
 
         resetbtn=tk.Button(text="Reset",font=SMALLFONT,command=self.reset_btn_cmd)
-        resetbtn.grid(row=5,column=0)
+        resetbtn.grid(row=6,column=0)
+        
     def radio_btn_gen(self):
+        self.questionlabel.config(text=""+self.qNa[self.page][0])
         self.radiobtnsframe=tk.Frame(relief="flat",borderwidth=2)
-        self.questionlabel.config(text=self.qNa[self.page][0])
         for i in self.qNa[self.page][1:]:#Iterates through inner lists of qNa 
             self.rb = tk.Radiobutton(master=self.radiobtnsframe,variable = self.v, value = i, 
             text= i,indicatoron=False,font=SMALLFONT)
             self.rblist.append(self.rb)
             self.rb.pack(fill="both")
-            self.radiobtnsframe.grid(row=0,column=0)
+            self.radiobtnsframe.grid(row=1,column=0)
         self.v.set("n/a")
 
     def nxt_btn_cmd(self):
